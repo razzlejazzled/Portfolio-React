0d5fb2ff5fff1de4d0cefa22bcc2667175a5c715
RazzleJazzled <robertjgassner@gmail.com>
2020-10-07 15:14:29 -0700

    text center

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 
+
 function Footer() {
     return (
     <div class="card">
-    <div class="card-header">
-      Quote
+    <div class="card-header text-center">
+      Robert "R.J." Gassner: Web Developer, Musicologist, Music Educator. 
     </div>
     <div class="card-body">
-      <blockquote class="blockquote mb-0">
+      <blockquote class="blockquote mb-0 text-center">
         <p>“It's a magical world, Hobbes, ol' buddy... Let's go exploring!”</p>
         <footer class="blockquote-footer">Bill Waterson <cite title="Source Title">Calvin and Hobbes</cite></footer>
       </blockquote>
