ba7565107af2a42825b97b55622553fe91681a02
RazzleJazzled <robertjgassner@gmail.com>
2020-10-07 15:17:12 -0700

    added changes

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -5,12 +5,13 @@ import Footer from "../Footer";
 function Contact() {
     return (
         <div>
-       
+
             <h1> Contact Information </h1>
             <div id="contact-info" className="container-sm">
-                <h1>Or use the following information:</h1>
+
                 <div className="row">
-                    <h5>Profiles:</h5> </div>
+                    <h3>Profiles:</h3>
+                </div>
                 <div><a href="https://github.com/razzlejazzled" class="other-info">Github</a> </div>
                 <div><a href="https://www.linkedin.com/in/r-j-gassner-371b241aa/" className="other-info">Linkedin</a>
                 </div>
